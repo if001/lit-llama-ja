9facb272ee7e7fab60efba5636b2c579aa9cf499
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-17 18:24:10 +0900

    debug

--- pretrain/redpajama.py
+++ pretrain/redpajama.py
@@ -266,8 +266,10 @@ def create_dataloader(
     datasets = []
     for prefix, _ in data_config:
         filenames = glob.glob(os.path.join(data_dir, prefix + "*"))
-        dataset = PackedDataset(
-            filenames, n_chunks=4, block_size=block_size, shuffle=shuffle, seed=seed,
+        n_chunks = len(filenames)
+        # n_chunks = 4 ## default
+        dataset = PackedDataset(            
+            filenames, n_chunks=n_chunks, block_size=block_size, shuffle=shuffle, seed=seed,
             num_processes=fabric.world_size, process_rank=fabric.global_rank, wrap=True
         )
         datasets.append(dataset)
