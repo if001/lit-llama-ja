acd7628c70828afc95e0cc64eb443d89797de4b1
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-21 18:58:39 +0900

    fix max iter

--- pretrain/redpajama.py
+++ pretrain/redpajama.py
@@ -55,8 +55,8 @@ log_interval = 100
 learning_rate = 0.0008
 batch_size = 125
 micro_batch_size = 5
-max_iters = 80000  # num_epochs * (epoch_size // micro_batch_size) // devices
-# max_iters = 143000  # num_epochs * (epoch_size // micro_batch_size) // devices
+# max_iters = 80000  # num_epochs * (epoch_size // micro_batch_size) // devices
+max_iters = 143000  # num_epochs * (epoch_size // micro_batch_size) // devices
 weight_decay = 0.1
 beta1 = 0.9
 beta2 = 0.95
@@ -168,7 +168,9 @@ def main(
     gradient_accumulation_iters = process_batch_size // micro_batch_size    
 
     train(fabric, model, optimizer, train_dataloader, val_dataloader, gradient_accumulation_iters, devices, out_dir)
-
+    fabric.print(f"Saving checkpoint to {out_dir}")
+    save_model_checkpoint(fabric, model, os.path.join(out_dir, f"iter-{max_iters:06d}-ckpt.pth"))
+                
 
 def train(
     fabric: L.Fabric,
