b3ec71ca09b5f9819e0b9f4ac7584f266d1e3904
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-26 21:00:36 +0900

    for llama2

--- generate.py
+++ generate.py
@@ -135,6 +135,10 @@ def main(
 
     model.eval()
     model = fabric.setup(model)
+     
+    with fabric.init_tensor():
+        # enable the kv cache
+        model.set_kv_cache(batch_size=1)
 
     # tokenizer = Tokenizer(tokenizer_path)
     tokenizer = HFTokenizer(tokenizer_path)
