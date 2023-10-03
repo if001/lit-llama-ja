614fd41831f794b798e88e2e148bf000dbb7984b
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-03 23:02:53 +0900

    fix

--- lit_llama/config_llama2.py
+++ lit_llama/config_llama2.py
@@ -1203,8 +1203,8 @@ phi = [
         vocab_size=35000,
         padded_vocab_size=35000,        
         block_size=2048,        
-        n_layer=16,
-        n_head=18,
+        n_layer=18,
+        n_head=20,
         n_embd=1020,
         rotary_percentage=0.5,  # 32 / (n_embd / n_head) = 32 / 64
         shared_attention_norm=True,
