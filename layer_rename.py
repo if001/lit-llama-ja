058de117a58962b411b4137fef7c9d0d0a53c0e0
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-04 15:40:30 +0900

    add fix layer rename

---
+++ layer_rename.py
@@ -0,0 +1,41 @@
+import torch
+from lit_llama.model_llama2 import GPT
+from lit_llama.config_llama2 import Llama2Config
+
+from lightning.fabric.utilities.load import _lazy_load
+
+def main(
+        model_size,        
+        checkpoint_file,
+        save_file
+):
+    print('load model: ', model_size)
+    print('checkpoint_file: ', checkpoint_file)
+    print('save_file: ', save_file)
+
+    config = Llama2Config.from_name(model_size)
+
+    model = GPT(config)
+    state_dict = _lazy_load(checkpoint_file)
+    model.load_state_dict(state_dict, strict=True)
+
+    compiled_model = torch.compile(model)
+
+    original_names = list(model.state_dict().keys())
+    compiled_names = list(compiled_model.state_dict().keys())
+
+    name_mapping = dict(zip(compiled_names, original_names))
+
+    new_state_dict = {}
+    for compiled_name, param in compiled_model.state_dict().items():
+        new_name = name_mapping[compiled_name]
+        new_state_dict[new_name] = param
+
+    compiled_model.load_state_dict(new_state_dict)
+    
+    torch.save(compiled_model.state_dict(), save_file)
+
+
+if __name__ == '__main__':
+    from jsonargparse.cli import CLI
+    CLI(main)    
