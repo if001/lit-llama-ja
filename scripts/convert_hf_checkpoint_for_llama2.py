ea3c5ffb1752057c00711bbb21c09fc7f64347e9
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-10 20:18:56 +0900

    fix

--- scripts/convert_hf_checkpoint_for_llama2.py
+++ scripts/convert_hf_checkpoint_for_llama2.py
@@ -230,10 +230,7 @@ def check_conversion_supported(lit_weights: Dict[str, torch.Tensor]) -> None:
 
 
 @torch.inference_mode()
-def convert_lit_checkpoint(checkpoint_path: Path, output_path: Path, config_path: Path) -> None:
-    # config = Llama2Config.from_json(config_path)
-    config = Llama2Config.from_name(config_path)
-
+def convert_lit_checkpoint(config: Llama2Config, output_path: Path, checkpoint_path: Path) -> None:    
     if "falcon" in config.name:
         copy_fn = partial(copy_weights_falcon, config.name)
     elif config._mlp_class == "LLaMAMLP":
@@ -251,6 +248,10 @@ def convert_lit_checkpoint(checkpoint_path: Path, output_path: Path, config_path
         gc.collect()
         saver.save(sd)
 
+def main(checkpoint_path: Path, output_path: Path, config_path: Path) -> None:
+    # config = Llama2Config.from_json(config_path)
+    config = Llama2Config.from_name(config_path)
+    convert_lit_checkpoint(config, output_path, checkpoint_path)
 
 if __name__ == "__main__":
     from jsonargparse import CLI
