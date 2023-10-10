1ed8d7c88af6204eb00d2784b4e9c928b092b875
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-10 20:23:12 +0900

    fix

--- lit_llama/config_llama2.py
+++ lit_llama/config_llama2.py
@@ -83,9 +83,6 @@ class Llama2Config:
 
     @classmethod
     def from_name(cls, name: str, **kwargs: Any) -> Self:
-        print('debug', name_to_config)
-        print('debug222', name in name_to_config)
-        print('debug222', name_to_config[name])
         conf_dict = name_to_config[name].copy()
         if "condense_ratio" in kwargs:  # legacy name
             kwargs["rope_condense_ratio"] = kwargs.pop("condense_ratio")
