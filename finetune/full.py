6772394c8ab8d22c2c30a7ce39b3f6ee62fbc902
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-25 02:25:40 +0900

    fix

--- finetune/full.py
+++ finetune/full.py
@@ -40,7 +40,7 @@ devices = 1
 
 # Hyperparameters
 # learning_rate = 3e-5
-learning_rate = 1e-5
+learning_rate = 1e-6
 batch_size = 128 / devices
 micro_batch_size = 1
 gradient_accumulation_iters = batch_size // micro_batch_size
@@ -49,9 +49,9 @@ assert gradient_accumulation_iters > 0
 epoch_size = 120000  # train dataset size
 num_epochs = 10
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
-weight_decay = 0.0001
+weight_decay = 0.001
 block_size = 2048
-warmup_iters = 1000
+warmup_iters = 500
 
 
 def main(
