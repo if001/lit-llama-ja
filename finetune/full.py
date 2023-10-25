34e17ea0f2f6ffcb6ff3070278730105f3fca542
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-25 21:13:21 +0900

    fix

--- finetune/full.py
+++ finetune/full.py
@@ -40,7 +40,7 @@ devices = 1
 
 # Hyperparameters
 # learning_rate = 3e-5
-learning_rate = 1e-6
+learning_rate = 3e-5
 batch_size = 128 / devices
 micro_batch_size = 1
 gradient_accumulation_iters = batch_size // micro_batch_size
@@ -49,7 +49,7 @@ assert gradient_accumulation_iters > 0
 epoch_size = 120000  # train dataset size
 num_epochs = 10
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
-weight_decay = 0.001
+weight_decay = 0.01
 block_size = 2048
 warmup_iters = 500
 
@@ -195,6 +195,15 @@ def validate(fabric: L.Fabric, model: torch.nn.Module, val_data: np.ndarray) -> 
 
     # produce an example:
     instruction = "週末に見るべき映画を推薦し、その理由を説明してください。"
+    with fabric.init_tensor():
+        # do not set `max_seq_length=max_returned_token` because memory is not a concern here
+        model.set_kv_cache(batch_size=1)
+
+    output = generate_response(model, instruction)
+    #fabric.print(instruction)
+    fabric.print(output)
+
+    instruction = "優れたポーカープレーヤーの特徴は何ですか"
     with fabric.init_tensor():
         # do not set `max_seq_length=max_returned_token` because memory is not a concern here
         model.set_kv_cache(batch_size=1)
