80c1863391c647d526aa0a22deb4731db7501993
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-26 21:45:20 +0900

    fix add v2 param

--- finetune/full.py
+++ finetune/full.py
@@ -40,8 +40,18 @@ devices = 1
 
 # Hyperparameters
 # learning_rate = 3e-5
-learning_rate = 4e-5
-batch_size = 128 / devices
+
+## 400M v1
+# learning_rate = 4e-5
+# batch_size = 128 / devices
+# weight_decay = 0.001
+# warmup_iters = 2000
+
+learning_rate = 8e-5
+batch_size = 512 / devices
+weight_decay = 0.01
+warmup_iters = 2000
+
 micro_batch_size = 1
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
@@ -49,7 +59,6 @@ assert gradient_accumulation_iters > 0
 epoch_size = 150000  # train dataset size
 num_epochs = 10
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
-weight_decay = 0.001
 block_size = 2048
 warmup_iters = 2000
 
