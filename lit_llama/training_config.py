1533cc562f9e5f77ccd8e054a756a6b32efb37dc
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-28 21:37:11 +0900

    fix

--- lit_llama/training_config.py
+++ lit_llama/training_config.py
@@ -145,8 +145,8 @@ class TrainingConfig():
             max_iters = 453000
             conf = dict(
                 model_size=model_size,
-                learning_rate=3e-4,
-                min_lr=3e-5,
+                learning_rate=1e-4,
+                min_lr=1e-5,
                 batch_size=256,
                 micro_batch_size=1,
                 max_iters=max_iters,
