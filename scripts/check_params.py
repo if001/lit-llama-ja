5be26ab11b607e08e817f264264f9ebc73dcc0f7
windows_on_wsl <otomijuf.004@gmail.com>
2023-10-31 20:44:52 +0900

    fix

--- scripts/check_params.py
+++ scripts/check_params.py
@@ -1,5 +1,7 @@
 import glob
 import json
+import matplotlib.pyplot as plt
+
 
 def main(
         target_dir: str = "./",
@@ -7,11 +9,33 @@ def main(
 ):    
     d = f"{target_dir}/search_param-*.json"
     files = glob.glob(d)
+    labels = []
+    loss_arr = []
+    val_loss_arr = []
     for file in files:        
-        with open(file, 'r') as f:
-            json_load = json.load(f)
-            print(json_load)
-            print('-'*100)
+        with open(file, 'r') as f:            
+            obj = json.load(f)
+            loss_arr.append(float(obj['loss']))
+            val_loss_arr.append(float(obj['val_loss']))
+            iter = obj['iter'])
+            file_name = file.replace(file, "search_param-")
+            labels.append(f'{iter}-{file_name}')
+
+
+    fig = plt.figure()    
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+    
+
+    ax1.grid()
+    t = range(len(labels))
+    ax1.plot(t, loss_arr, label="loss")
+    ax1.set_xticklabels(labels)
+
+    ax2.grid()
+    ax2.plot(t, val_loss_arr, label="val_loss")
+    ax2.set_xticklabels(labels)
+    plt.show()
 
 if __name__ == "__main__":
     from jsonargparse.cli import CLI
