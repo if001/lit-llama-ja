c4f29a2a12f481209420cc922660a0f0257f20b8
windows_on_wsl <otomijuf.004@gmail.com>
2023-11-02 20:59:33 +0900

    search param lr

--- pretrain/seach_hyper_param.py
+++ pretrain/seach_hyper_param.py
@@ -9,15 +9,16 @@ base_cmd=[
     "--val_data_dir data/ja_data",
     "--model_size \"Llama-2-400M-hf\"",
     "--out_dir \"/content/drive/MyDrive/pre_trained/llama2/400M_search\"",
-    "--log_dir \"/content/drive/MyDrive/pre_trained/llama2/400M_search/logs\"",
+    "--log_dir \"/content/drive/MyDrive/pre_trained/llama2/400M_search/logs_lr\"",
     "--interrupt" "true"
 ]
 
 # batchs = [16, 32, 64, 128, 256]
 # batchs = [16, 64, 256]
-batchs = [2, 4, 8]
+# batchs = [2, 4, 8]
+batchs = [4]
 # lrs = [1e-4, 5e-4, 1e-5, 5e-5, 1e-6]
-lrs = [1e-4, 1e-5, 1e-6]
+lrs = [1e-3, 1e-4, 1e-5]
 
 # weight_decays = [0.1, 0.01, 0.001, 0.0001]
 weight_decays = [0.001]
