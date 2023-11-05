22c230012f86c2541de9375525a5020618d9ffc1
windows_on_wsl <otomijuf.004@gmail.com>
2023-11-05 19:08:03 +0900

    fix

--- lit_llama/config_llama2.py
+++ lit_llama/config_llama2.py
@@ -1377,9 +1377,9 @@ phi = [
         vocab_size=35000,
         padded_vocab_size=35000,
         block_size=2048,
-        n_layer=16,
-        n_head=16,
-        n_embd=1600,
+        n_layer=18,
+        n_head=18,
+        n_embd=1080,
         rotary_percentage=0.5,  # 32 / (n_embd / n_head) = 32 / 64
         shared_attention_norm=True,
         lm_head_bias=True,
