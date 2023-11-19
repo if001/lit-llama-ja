7516323ff7b5aa3884758c77e6acca79a99f9f00
windows_on_wsl <otomijuf.004@gmail.com>
2023-11-19 15:30:09 +0900

    add top_p

--- generate.py
+++ generate.py
@@ -6,6 +6,9 @@ from typing import Optional
 
 import lightning as L
 import torch
+from torch import Tensor
+from torch.nn import functional as F
+
 
 # support running without installing as a package
 wd = Path(__file__).parent.parent.resolve()
@@ -16,6 +19,47 @@ from lit_llama import GPT
 from lit_llama.utils import lazy_load, llama_model_lookup, quantization
 
 
+def top_k_top_p_filtering(
+    logits: Tensor,
+    top_k: int = 0,
+    top_p: float = 1.0,
+    filter_value: float = -float("Inf"),
+    min_tokens_to_keep: int = 1,
+) -> Tensor:
+    """Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+    Args:
+        logits: logits distribution shape (batch size, vocabulary size)
+        if top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+        if top_p < 1.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+            Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+        Make sure we keep at least min_tokens_to_keep per batch example in the output
+    From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+
+    if top_k > 0:
+        top_k = min(max(top_k, min_tokens_to_keep), logits.size(-1))  # Safety check
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p < 1.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
+        sorted_indices_to_remove = cumulative_probs > top_p
+        if min_tokens_to_keep > 1:
+            # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
+            sorted_indices_to_remove[..., :min_tokens_to_keep] = 0
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        # scatter sorted tensors to original indexing
+        indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+        logits[indices_to_remove] = filter_value
+    return logits    
+
 @torch.no_grad()
 def generate(
     model: GPT,
@@ -24,7 +68,8 @@ def generate(
     *,
     max_seq_length: Optional[int] = None,
     temperature: float = 1.0,
-    top_k: Optional[int] = None,
+    top_k: int = 0,
+    top_p: int = 1.0,
     eos_id: Optional[int] = None,
 ) -> torch.Tensor:
     """Takes a conditioning sequence (prompt) as input and continues to generate as many tokens as requested.
@@ -66,10 +111,11 @@ def generate(
         logits = model(x, input_pos)
         logits = logits[0, -1] / temperature
 
-        # optionally crop the logits to only the top k options
-        if top_k is not None:
-            v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
-            logits = torch.where(logits < v[[-1]], -float("Inf"), logits)
+        logits = top_k_top_p_filtering(logits, top_k, top_p)
+        # # optionally crop the logits to only the top k options
+        # if top_k is not None:
+        #     v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+        #     logits = torch.where(logits < v[[-1]], -float("Inf"), logits)
 
         probs = torch.nn.functional.softmax(logits, dim=-1)
         idx_next = torch.multinomial(probs, num_samples=1).to(dtype=dtype)
