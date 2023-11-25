58b4a64f9a4a90e7e0d90d48a0b0bd5be4e94550
windows_on_wsl <otomijuf.004@gmail.com>
2023-11-25 22:31:24 +0900

    fix

--- heat_map.py
+++ heat_map.py
@@ -44,10 +44,8 @@ def gen(
     hook.remove()
     q, k, v = attention
     
-    attention_weights = []
-    k = k[1:]
-    print('k ', k.shape)
-    for _, k_part in enumerate(k):
+    attention_weights = []    
+    for _, k_part in enumerate(k[0]):
         print('k.part ', k_part.shape)
         _k = torch.transpose(k_part, 0, 1) ## 1, num_heads, seq_len, hidden_dim => hidden_dim, seq_len
         attention_weight = torch.matmul(q, _k) / np.sqrt(q.size(-1))
