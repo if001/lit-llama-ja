56b2ded79d875c95c9f95e3dcc586d723bc20e8d
windows_on_wsl <otomijuf.004@gmail.com>
2023-11-29 21:04:12 +0900

    debug

--- lit_llama/model_llama2.py
+++ lit_llama/model_llama2.py
@@ -385,6 +385,8 @@ class CausalSelfAttention(nn.Module):
         attn_weight = query @ key.transpose(-2, -1) * scale_factor        
         print('attn_weight', attn_weight)
         print('attn_bias', attn_bias)
+        attn_bias = attn_bias.to(device=attn_weight.device)
+        print('attn_bias2', attn_bias)
         attn_weight += attn_bias
         attn_weight = attn_weight * scale_tensor ## アダマール積を取ることでscaleする        
         attn_weight = torch.softmax(attn_weight, dim=-1)
