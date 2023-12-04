5c5c48c8f04a227c18d6f838386226b1f1a0e7a9
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-04 15:53:53 +0900

    debug

--- lit_llama/model_llama2.py
+++ lit_llama/model_llama2.py
@@ -391,8 +391,11 @@ class CausalSelfAttention(nn.Module):
         attn_weight = query @ key.transpose(-2, -1) * scale_factor
         # print('attn_weight', attn_weight.shape)
         attn_weight += attn_bias
-        attn_weight += scale_tensor ## scaleする        
+        print('attn_weight', attn_weight[0, 0, 0, :10])
+        attn_weight += scale_tensor ## scaleする
+        print('scale_tensor', attn_weight[0, 0, 0, :10])
         attn_weight = torch.softmax(attn_weight, dim=-1)
+        print('attn_weight2', attn_weight[0, 0, 0, :10])
         attn_weight = torch.dropout(attn_weight, dropout_p, train=True)        
         return attn_weight @ value
 
