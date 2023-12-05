1b0777d16a6e7c4552dcb4771f2cc82b78e2d283
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-05 18:38:45 +0900

    fix

--- evaluate/eval_next_prob.py
+++ evaluate/eval_next_prob.py
@@ -134,7 +134,7 @@ def generate(
         
         probs = torch.nn.functional.softmax(next_token_scores, dim=-1)
 
-        print('probs', probs)
+        print('probs', probs.shape, probs)
 
         idx_next = torch.multinomial(probs, num_samples=1)
         idx_next = idx_next.to(dtype=dtype)
