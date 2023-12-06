4e3ccbab99f0abd20ef6ba1884f830d015145437
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-06 19:09:39 +0900

    fix

--- evaluate/eval_jcommonsenseQA.py
+++ evaluate/eval_jcommonsenseQA.py
@@ -101,7 +101,7 @@ def generate(
         next_token_scores = next_token_scores.squeeze(0)
 
         probs = torch.nn.functional.softmax(next_token_scores, dim=-1)
-        idx_next = torch.multinomial(probs, num_samples=1)        
+        idx_next = torch.multinomial(probs, num_samples=1)
         idx_next = idx_next.to(dtype=dtype)
 
         # advance
