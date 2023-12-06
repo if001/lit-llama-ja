7b4883bd99ea2ffba39effeadc3c359967e6c488
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-06 21:34:01 +0900

    debu

--- lit_llama/model_llama2.py
+++ lit_llama/model_llama2.py
@@ -103,7 +103,8 @@ class GPT(nn.Module):
                 if self.mask_cache is None:
                     raise TypeError("You need to call `gpt.set_kv_cache()`")
                 mask = self.mask_cache.index_select(2, input_pos)
-            else:            
+                print('mask', mask.shape, mask)
+            else:
                 cos = self._cos_list[i][:T]
                 sin = self._sin_list[i][:T]
                 mask = None
