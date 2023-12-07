e26560918175615a60cf64041fe609b01b79c862
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-07 14:44:27 +0900

    fix

--- hf_heat_map.py
+++ hf_heat_map.py
@@ -15,8 +15,8 @@ def main(
     
     tokens = tokenizer.encode_plus(prompt, return_tensors='pt', add_special_tokens=True, max_length=512, truncation=True)
 
-    outputs = model(tokens['input_ids'], attention_mask=tokens['attention_mask'])
-    print('outputs.attentions', outputs)
+    outputs = model(tokens['input_ids'], attention_mask=tokens['attention_mask'], output_attentions=True)
+    print('outputs.attentions', outputs.attentions)
     attention = torch.mean(outputs.attentions[-1], dim=1)[0].detach().numpy()
 
     sns.heatmap(attention, cmap="YlGnBu", xticklabels=tokenizer.convert_ids_to_tokens(tokens['input_ids'][0]), yticklabels=tokenizer.convert_ids_to_tokens(tokens['input_ids'][0]))
