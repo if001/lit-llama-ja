b1861fd7090ecc79fcfa3aa39b368bd90bf3b505
windows_on_wsl <otomijuf.004@gmail.com>
2023-12-19 12:49:28 +0900

    fix

--- evaluate/eval_next_prob.py
+++ evaluate/eval_next_prob.py
@@ -94,7 +94,7 @@ def show_graph(probs, save_fig_name):
 
     # グラフの描画
     plt.figure(figsize=(12, 8))
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_shape='s',
+    nx.draw(G, pos, with_labels=True, node_color='lightblue',
             node_size=2000, font_size=10, font_weight='bold', font_family='IPAexGothic')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
     plt.title("")
