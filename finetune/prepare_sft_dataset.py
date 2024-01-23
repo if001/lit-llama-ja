61f7a8247321dc776e2e76d8a8c4c49013a92cc2
windows_on_wsl <otomijuf.004@gmail.com>
2024-01-24 00:06:34 +0900

    fix

--- finetune/prepare_sft_dataset.py
+++ finetune/prepare_sft_dataset.py
@@ -1,6 +1,10 @@
-from datasets import load_dataset, concatenate_datasets, load_from_disk 
-from trl.trainer.utils import _prepare_dataset
+from datasets import Dataset, load_dataset, concatenate_datasets, load_from_disk 
 from transformers import AutoTokenizer
+
+from trl.trainer.utils import ConstantLengthDataset
+from datasets.builder import DatasetGenerationError
+from datasets.arrow_writer import SchemaInferenceError
+
 
 """sample
 
@@ -37,6 +41,53 @@ def format_instruction(ds):
         ds['text'] = text
         return ds
     
+def _prepare_packed_dataloader(        
+        tokenizer,
+        dataset,
+        dataset_text_field,
+        max_seq_length,
+        num_of_sequences,
+        chars_per_token,
+        formatting_func=None,
+        append_concat_token=True,
+        add_special_tokens=True,
+    ):
+    if dataset_text_field is not None or formatting_func is not None:
+        if tokenizer is None:
+            raise ValueError("You need to pass a tokenizer when using `dataset_text_field` with `SFTTrainer`.")
+
+        constant_length_iterator = ConstantLengthDataset(
+                tokenizer,
+                dataset,
+                dataset_text_field=dataset_text_field,
+                formatting_func=formatting_func,
+                seq_length=max_seq_length,
+                infinite=False,
+                num_of_sequences=num_of_sequences,
+                chars_per_token=chars_per_token,
+                eos_token_id=tokenizer.eos_token_id,
+                append_concat_token=append_concat_token,
+                add_special_tokens=add_special_tokens,
+        )
+
+        def data_generator(constant_length_iterator):
+            for i in constant_length_iterator:
+                yield i
+
+        try:
+            packed_dataset = Dataset.from_generator(
+                data_generator, gen_kwargs={"constant_length_iterator": constant_length_iterator}
+            )
+        except (DatasetGenerationError, SchemaInferenceError):
+            raise ValueError(
+                "Error occurred while packing the dataset. Make sure that your dataset has enough samples to at least yield one packed sequence."
+            )
+        return packed_dataset
+    else:
+        raise ValueError(
+            "You need to pass a `dataset_text_field` or `formatting_func` argument to the SFTTrainer if you want to use the `ConstantLengthDataset`."
+        )
+            
 def main(
         tokenizer_name: str = "",
         dataset_name: str = "",
@@ -62,33 +113,27 @@ def main(
         dataset = dataset.map(format_instruction)
         
     dataset = dataset.shuffle().train_test_split(test_size=0.1)
-
-    packing=True
+        
     dataset_text_field="text"
-    max_seq_length=2048
-    formatting_func=None
+    max_seq_length=2048    
     num_of_sequences=1024
     chars_per_token=3.6
     remove_unused_columns=True
 
-    train_data = _prepare_dataset(
+    train_data = _prepare_packed_dataloader(
         dataset["train"], 
-        tokenizer, 
-        packing, 
+        tokenizer,         
         dataset_text_field,
-        max_seq_length,
-        formatting_func,
+        max_seq_length,        
         num_of_sequences,
         chars_per_token,
         remove_unused_columns
         )
-    test_data = _prepare_dataset(
+    test_data = _prepare_packed_dataloader(
         dataset["text"], 
-        tokenizer, 
-        packing,
+        tokenizer,         
         dataset_text_field,
-        max_seq_length,
-        formatting_func,        
+        max_seq_length,        
         num_of_sequences,
         chars_per_token,
         remove_unused_columns
