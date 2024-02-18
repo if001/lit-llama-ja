cbd8ab1d09bba77b55758524bdc5be80224a4163
windows_on_wsl <otomijuf.004@gmail.com>
2024-02-18 14:58:37 +0900

    debug

--- lit_llama/packed_dataset.py
+++ lit_llama/packed_dataset.py
@@ -189,9 +189,8 @@ class PackedDatasetIterator:
                 raise StopIteration
             else:
                 self._file_idx = 0
-        print('len self._filenames...', len(self._filenames))
+
         for i in range(self._n_chunks):
-            print('self._file_idx + i', self._file_idx + i)
             filename = self._filenames[self._file_idx + i]
             if self._dtype is None:
                 self._dtype, self._chunk_size = self._read_header(
