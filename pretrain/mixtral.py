edbf9a3643c2d6fb66c212e011d64c52c958657d
windows_on_wsl <otomijuf.004@gmail.com>
2024-02-20 18:25:51 +0900

    debug

--- pretrain/mixtral.py
+++ pretrain/mixtral.py
@@ -372,8 +372,8 @@ def create_dataloader(
     
     for prefix, _ in data_config:
         filenames = glob.glob(os.path.join(data_dir, prefix + "*"))
-        n_chunks = len(filenames) - 1
-        # n_chunks = 4 ## default
+        # n_chunks = len(filenames) - 1
+        n_chunks = 4 ## default
         dataset = PackedDataset(
             filenames, n_chunks=n_chunks, block_size=block_size, shuffle=shuffle, seed=seed,
             num_processes=fabric.world_size, process_rank=fabric.global_rank, wrap=True
