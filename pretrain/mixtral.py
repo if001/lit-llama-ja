06aa3f868b4a7bd250b2560afe422947b555c94d
windows_on_wsl <otomijuf.004@gmail.com>
2024-02-20 17:43:07 +0900

    debug

--- pretrain/mixtral.py
+++ pretrain/mixtral.py
@@ -222,8 +222,7 @@ def train(
     # eval_interval = 4096 / trainingConfig.batch_size
     # save_interval = 500
     # eval_interval = 100
-    save_interval = 1024/4
-    eval_interval = 1024/4
+    eval_interval = 100
     log_interval = 100
     total_time = 0
 
